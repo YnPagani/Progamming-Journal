286180fefc558ac41cad29af180bd928b118837e
Yuri Pagani <yuripagani@gmail.com>
2020-11-19 18:45:39 -0300

    Add menu and main app loop

---
+++ app.py
@@ -0,0 +1,20 @@
+menu = """
+Please select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+Your selection: 
+"""
+
+print("Welcome to the Progamming Journal!")
+
+user_input = input(menu)
+
+while (user_input := input(menu)) != "3":
+    if user_input == "1":
+        pass
+    elif user_input == "2":
+        pass
+    else:
+        pass
