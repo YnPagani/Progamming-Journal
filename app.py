599b95c245beae203ad7939a0e6293559d6042dd
Yuri Pagani <yuripagani@gmail.com>
2020-12-23 17:01:45 -0300

    Add database prototype with python lists

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
+import db
+
+
+def prompt_new_entry():
+    # Asking user for data
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date: ")
+
+    # Adding data to db
+    db.add_entry(entry_content, entry_date)
+
+
+def view_entries(entries: list):
+    # Retrieving data from db and displaying it
+    for entry in entries:
+        print(f"{entry['content']}\n{entry['date']}\n\n")
+
+
 menu = """
 Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
 3) Exit.
 
-Your selection: 
-"""
+Your selection: """
 
-print("Welcome to the Progamming Journal!")
-
-user_input = input(menu)
+print("Welcome to the Programming Journal!")
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        pass
+        prompt_new_entry()
+
     elif user_input == "2":
-        pass
+        view_entries(db.get_entries())
+
     else:
-        pass
+        print("Invalid input, try again")
---
+++ db.py
@@ -0,0 +1,11 @@
+entries = []
+
+
+def add_entry(entry_content: str, entry_date: str):
+    entries.append({"content": entry_content,
+                    "date": entry_date})
+
+
+def get_entries() -> list:
+    return entries
+
